27c31a842dab3bc5627933a4f77c3ed16284fb8e
Austin Day <Austin Day>
2017-03-18 19:36:03 -0400

    travis added

--- test_rpn.py
+++ test_rpn.py
@@ -1,10 +1,16 @@
 import unittest
 import rpn
 
+
 class TestBasics(unittest.TestCase):
-	def test_add(self):
-		result = rpn.calculate('1 1 +')
-		self.assertEqual(2, result)
-	def test_subtract(self):
-		result = rpn.calculate('5 3 -')
-		self.assertEqual(2, result)
+    def test_add(self):
+        result = rpn.calculate('1 1 +')
+        self.assertEqual(2, result)
+
+    def test_add_custom(self):
+        result = rpn.calculate('0 25 +')
+        self.assertEquals(25, result)
+
+    def test_subtract(self):
+        result = rpn.calculate('5 3 -')
+        self.assertEqual(2, result)
